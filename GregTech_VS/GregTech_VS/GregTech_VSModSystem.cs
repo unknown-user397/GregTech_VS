044e4bde228079333f2b301b27169a01e0b1b384
ecask <josh.caskey2007@gmail.com>
2024-11-16 17:32:54 -0500

    Add project files.

---
+++ GregTech_VS/GregTech_VS/GregTech_VSModSystem.cs
@@ -0,0 +1,27 @@
+﻿using GregTech_VS.assets.gregtech_vs.machine.steam;
+using Vintagestory.API.Client;
+using Vintagestory.API.Common;
+using Vintagestory.API.Config;
+using Vintagestory.API.Server;
+namespace GregTech_VS
+{
+    public class GregTech_VSModSystem : ModSystem
+    {
+
+        // Called on server and client
+        // Useful for registering block/entity classes on both sides
+        public override void Start(ICoreAPI api)
+        {
+            api.RegisterBlockClass(Mod.Info.ModID + ".lowsolidsteamboiler", typeof(BlockLowSolidSteamBoiler));
+        }
+
+        public override void StartServerSide(ICoreServerAPI api)
+        {
+        }
+
+        public override void StartClientSide(ICoreClientAPI api)
+        {
+        }
+
+    }
+}
---
+++ GregTech_VS/GregTech_VS/src/machine/steam/BlockLowSolidSteamBoiler.cs
@@ -0,0 +1,19 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.MathTools;
+
+namespace GregTech_VS.assets.gregtech_vs.machine.steam
+{
+    internal class BlockLowSolidSteamBoiler : Block
+    {
+        public override void OnBlockPlaced(IWorldAccessor world, BlockPos blockPos, ItemStack byItemStack = null)
+        {
+            api.Logger.Event("LowSolidSteamBoiler Block Placed!");
+            base.OnBlockPlaced(world, blockPos, byItemStack);
+        }
+        public override void OnBlockBroken(IWorldAccessor world, BlockPos pos, IPlayer byPlayer, float dropQuantityMultiplier = 1)
+        {
+            api.Logger.Event("LowSolidSteamBoiler Block Broken!");
+            base.OnBlockBroken(world, pos, byPlayer, dropQuantityMultiplier);
+        }
+    }
+}
